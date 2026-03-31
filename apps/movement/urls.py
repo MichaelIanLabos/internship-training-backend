bfb9c5617b7c69a55f49211157d58a2f24a3b250
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-31 11:25:49 +0800

    feat: added rejection endpoint for rejection

--- apps/movement/urls.py
+++ apps/movement/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MovementAPIView
+from .views import MovementAPIView, MovementApprovalAPIView, MovementRejectionAPIView
 
 urlpatterns = [
     path('', MovementAPIView.as_view(), name='movement-list-create'),
+    path('<int:pk>/approve/', MovementApprovalAPIView.as_view(), name='movement-approve'),
+    path('<int:pk>/reject/', MovementRejectionAPIView.as_view(), name='movement-reject'),
 ]
