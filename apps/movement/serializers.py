d7abce13cd1e52a4166ab188e06721d8e818572f
BweepBwop <a.kelsey.magat@gmail.com>
2026-04-01 16:01:18 +0800

    feat: add nested employee and user name fields to MovementSerializer

--- apps/movement/serializers.py
+++ apps/movement/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 from .models import EmployeeMovement
-from apps.employee.models import Employee
+
 
 class MovementSerializer(serializers.ModelSerializer):
+    employee_code = serializers.CharField(source='employee.employee_code', read_only=True)
+    employee_first_name = serializers.CharField(source='employee.first_name', read_only=True)
+    employee_last_name = serializers.CharField(source='employee.last_name', read_only=True)
+    requested_by_first_name = serializers.CharField(source='requested_by.first_name', read_only=True)
+    requested_by_last_name = serializers.CharField(source='requested_by.last_name', read_only=True)
+    approved_by_first_name = serializers.CharField(source='approved_by.first_name', read_only=True, default='')
+    approved_by_last_name = serializers.CharField(source='approved_by.last_name', read_only=True, default='')
+
     class Meta:
         model = EmployeeMovement
-        fields = '__all__'
-        read_only_fields = ['status', 'requested_by', 'approved_by', 'created_at', 'updated_at']
+        exclude = ['is_deleted']
+        read_only_fields = [
+            'status', 'requested_by', 'approved_by', 'created_at', 'updated_at',
+            'employee_code', 'employee_first_name', 'employee_last_name',
+            'requested_by_first_name', 'requested_by_last_name',
+            'approved_by_first_name', 'approved_by_last_name',
+        ]
