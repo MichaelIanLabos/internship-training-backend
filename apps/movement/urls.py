1fd144c740898105d6e6100bd473ebd7084bbca3
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-04-01 09:24:12 +0800

    feat: added approve endpoint for Approve Request

--- apps/movement/urls.py
+++ apps/movement/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovementAPIView, MovementDetailAPIView
+from .views import MovementAPIView, MovementDetailAPIView, MovementApproveAPIView
 
 urlpatterns = [
     path('', MovementAPIView.as_view(), name='movement-list-create'),
     path('<int:pk>/', MovementDetailAPIView.as_view(), name='movement-detail'),
+    path('<int:pk>/approve/', MovementApproveAPIView.as_view(), name='movement-approve'),
 ]
